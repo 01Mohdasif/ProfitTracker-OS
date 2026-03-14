09f855fe7c3ba223665b5158c0a62e3ac406603d
01Mohdasif <01mohdasif03@gmail.com>
2026-03-14 19:40:13 +0530

    style(sidebar): remove blank line and update logout text

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -101,9 +101,9 @@ export function AppSidebar({ role, user }: { role: Role; user: User }) {
               onClick={handleLogout}
               tooltip="Logout"
             >
-            
+
               <LogOut className="w-5 h-5" />
-              <span>Logout</span>
+              <span>Logout karo</span>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
