86dec490af2ae17c5b1e23e32c90f74bb54500d5
01Mohdasif <01mohdasif03@gmail.com>
2026-03-14 19:38:03 +0530

    style(sidebar): add blank line in logout button

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -101,6 +101,7 @@ export function AppSidebar({ role, user }: { role: Role; user: User }) {
               onClick={handleLogout}
               tooltip="Logout"
             >
+            
               <LogOut className="w-5 h-5" />
               <span>Logout</span>
             </SidebarMenuButton>
